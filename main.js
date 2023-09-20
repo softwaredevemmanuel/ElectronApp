36c2f76bfaa660fb1125585f9b9ab9a9c884795f
softwaredevemmanuel <48366297+softwaredevemmanuel@users.noreply.github.com>
2023-09-20 13:29:21 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+var path=require("path");
+
+
+function creatMainWindow(){
+    const mainWindow = new BrowserWindow({
+        title: 'Electron',
+        width: 1000,
+        height:850
+    });
+
+
+    const startUrl = url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file'
+    });
+
+
+    mainWindow.loadURL(startUrl)
+}
+
+app.whenReady().then(creatMainWindow)
